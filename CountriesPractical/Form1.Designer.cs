43eb3248abb7a0d52206c49e37514e71c8c918a5
Vlad <vsporyadin@gmail.com>
2023-12-07 18:30:51 +0300

    dn

--- CountriesPractical/Form1.Designer.cs
+++ CountriesPractical/Form1.Designer.cs
@@ -28,34 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
-            // dataGridView1
+            // listBox1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(450, 23);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(319, 403);
-            dataGridView1.TabIndex = 0;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(468, 25);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(302, 379);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 98);
+            button1.TabIndex = 1;
+            button1.Text = "Все страны";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 129);
+            button2.Name = "button2";
+            button2.Size = new Size(159, 98);
+            button2.TabIndex = 2;
+            button2.Text = "Все названия стран";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(178, 129);
+            button3.Name = "button3";
+            button3.Size = new Size(159, 98);
+            button3.TabIndex = 3;
+            button3.Text = "Все столицы";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 233);
+            button4.Name = "button4";
+            button4.Size = new Size(159, 88);
+            button4.TabIndex = 4;
+            button4.Text = "Все старны в Европе";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(178, 25);
+            button5.Name = "button5";
+            button5.Size = new Size(159, 98);
+            button5.TabIndex = 5;
+            button5.Text = "Все страны с площадью > 1000000";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(826, 460);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
--- CountriesPractical/Form1.cs
+++ CountriesPractical/Form1.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using System.Windows.Forms;
+using SQLitePCL;
+using CountriesPractical;
 
 namespace CountriesPractical
 {
@@ -12,34 +15,117 @@ namespace CountriesPractical
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //using (AppConect db = new AppConect())
+            //{
+
+            //    Country USA = new Country { Name = "USA", CapitalName = "Washington", Population = 331000000, Area = 9833520, PartOfTheWorld = "North America" };
+            //    Country Russia = new Country { Name = "Russia", CapitalName = "Moscaw", Population = 145000000, Area = 148940000, PartOfTheWorld = "Eurasia" };
+            //    Country Italy = new Country { Name = "Italy", CapitalName = "Rom", Population = 59000000, Area = 301230, PartOfTheWorld = "Europ" };
+            //    Country Mexico = new Country { Name = "Mexico", CapitalName = "MexicoCity", Population = 126000000, Area = 1972550, PartOfTheWorld = "South America" };
+            //    Country Japan = new Country { Name = "Japan", CapitalName = "Tokio", Population = 125000000, Area = 377973, PartOfTheWorld = "Asia" };
+            //    Country SouthAfrica = new Country { Name = "SouthAfrica", CapitalName = "CapeTown", Population = 59000000, Area = 1220000, PartOfTheWorld = "Africa" };
+
+            //    db.country.Add(USA);
+            //    db.country.Add(Russia);
+            //    db.country.Add(Italy);
+            //    db.country.Add(Mexico);
+            //    db.country.Add(Japan);
+            //    db.country.Add(SouthAfrica);
+            //    db.SaveChanges();
+            //    MessageBox.Show("Объекты успешно сохранены");
+
+            //}
+        }
+
+
+        public void ShowAllCointries()
+        {
             using (AppConect db = new AppConect())
             {
-                Country USA = new Country { Name = "USA", CapitalName = "Washington", Population = 331000000, Area = 9833520, PartOfTheWorld = "North America" };
-                Country Russia = new Country { Name = "Russia", CapitalName = "Moscaw", Population = 145000000, Area = 148940000, PartOfTheWorld = "Eurasia" };
-                Country Italy = new Country { Name = "Italy", CapitalName = "Rom", Population = 59000000, Area = 301230, PartOfTheWorld = "Europ" };
-                Country Mexico = new Country { Name = "Mexico", CapitalName = "MexicoCity", Population = 126000000, Area = 1972550, PartOfTheWorld = "South America" };
-                Country Japan = new Country { Name = "Japan", CapitalName = "Tokio", Population = 125000000, Area = 377973, PartOfTheWorld = "Asia" };
-                Country SouthAfrica = new Country { Name = "SouthAfrica", CapitalName = "CapeTown", Population = 59000000, Area = 1220000, PartOfTheWorld = "Africa" };
-
-                db.country.Add(USA);
-                db.country.Add(Russia);
-                db.country.Add(Italy);
-                db.country.Add(Mexico);
-                db.country.Add(Japan);
-                db.country.Add(SouthAfrica);
-                db.SaveChanges();
-                MessageBox.Show("Объекты успешно сохранены");
-
                 var countries = db.country.ToList();
-
-                foreach (Country c in countries)
-                {
-                    dataGridView1.DataSource = c;
-                }
-
+                listBox1.Items.AddRange(countries.ToArray());
             }
         }
+
+        public void ShowAllCointriesName()
+        {
+            using (AppConect db = new AppConect())
+            {
+                var countries = db.country.ToList();
+                listBox1.DisplayMember = "Name";
+                listBox1.Items.AddRange(countries.ToArray());
+            }
+        }
+
+        public void ShowAllCointriesCapitalName()
+        {
+            using (AppConect db = new AppConect())
+            {
+                var countries = db.country.ToList();
+                listBox1.DisplayMember = "CapitalName";
+                listBox1.Items.AddRange(countries.ToArray());
+            }
+        }
+
+        public void ShowAllCointriesInEurop()
+        {
+            using (AppConect db = new AppConect())
+            {
+                var europeanCountries = db.country.Where(c => c.PartOfTheWorld == "Europ").ToList();
+
+                listBox1.DisplayMember = "Name";
+                listBox1.Items.AddRange(europeanCountries.ToArray());
+            }
+        }
+
+        public void ShowAllCointriesWhreAreaMoreOneMilloin()
+        {
+            using (AppConect db = new AppConect())
+            {
+                var Countries = db.country.Where(c => c.Area >= 1000000).ToList();
+
+
+                var countries = db.country.ToList();
+                listBox1.Items.AddRange(countries.ToArray());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            ShowAllCointries();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            ShowAllCointriesName();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            ShowAllCointriesCapitalName();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            ShowAllCointriesInEurop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            ShowAllCointriesWhreAreaMoreOneMilloin();
+        }
     }
+
 
     public class Country
     {
@@ -50,9 +136,13 @@ namespace CountriesPractical
         public int Area { get; set; }
         public string PartOfTheWorld { get; set; }
 
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Capital: {CapitalName}, Population: {Population}, Area: {Area}, Part of the World: {PartOfTheWorld}";
+        }
     }
 
-    public class AppConect:DbContext
+    public class AppConect : DbContext
     {
         public DbSet<Country> country => Set<Country>();
 
@@ -64,5 +154,5 @@ namespace CountriesPractical
         }
     }
 
-
 }
+
